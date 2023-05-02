7b2e86c9a97d0f66479f668f982a9fa63eea57a6
kavishka <kaviya1234z@gmail.com>
2023-05-02 11:25:04 +0530

    fixed issues

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,14 +2,12 @@ const express = require("express")
 const userRouter = express.Router();
 const {signUp ,logIn ,logOut,getAllProducts,getUserDetails,getProductDetails,saveCartDetails,getCartItems,cartItemDelete} = require("../controller/user-contoller");
 const {userSignUpValidationRules,userSignInValidationRules,validateSignup,validateSignin} = require('../config/validator')
-const middleware = require('../middleware/middleware')
+
 
 
 
 userRouter.post("/login", [userSignInValidationRules(), validateSignin], logIn)
 userRouter.post("/signup", [userSignUpValidationRules(), validateSignup], signUp)
-// userRouter.post("/signup",[middle1ware.isNotLoggedIn,userSignUpValidationRules(),validateSignup],signUp)
-// userRouter.get("/getallproducts/:name",getAllProducts)
 userRouter.get("/logout", logOut)
 userRouter.get("/getallproducts",getAllProducts)
 userRouter.get("/getuserdetails/:email",getUserDetails)
